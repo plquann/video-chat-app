2b81f4298854e477883cd1fcfda9c76b7bd060f8
QuanKhs <quanphamluong@gmail.com>
2021-07-27 16:36:49 +0700

    socket connection client

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { Grid, Typography, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    video: {
+        width: '550px',
+        [theme.breakpoints.down('xs')]: {
+            width: '300px',
+        },
+    },
+    gridContainer: {
+        justifyContent: 'center',
+        [theme.breakpoints.down('xs')]: {
+            flexDirection: 'column',
+        },
+    },
+    paper: {
+        padding: '10px',
+        border: '2px solid black',
+        margin: '10px',
+    },
+}));
 
 const VideoPlayer = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            VideoPlayer
-        </div>
+        <Grid container className={classes.gridContainer}>
+            {/* our own video */}
+            
+        </Grid>
     )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ContextProvider } from './SocketContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
